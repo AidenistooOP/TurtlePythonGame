ac52a3c09f069d0799134a446ff28cc8a7ac5c4a
Aiden Onyenwenu <>
2023-04-03 12:10:28 +0000

    Initial commit

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import turtle
 
 def draw_shape(shape):
     for i in range(0, 4):
-        turtle.fd(50) if shape == "square" else turtle.fd(50), turtle.lt(
-            120) if shape == "triangle" else turtle.lt(90)
+        turtle.fd(50)
+        if shape == "square":
+            turtle.lt(90)
+        elif shape == "triangle":
+            turtle.lt(120)
     turtle.fd(75)
 
 
 turtle.penup()
-turtle.goto(0, 0)
+turtle.goto(-200, 0)
 turtle.pendown()
 
 for shape in ["square", "triangle", "square", "triangle"]:
     draw_shape(shape)
+
+turtle.done()
